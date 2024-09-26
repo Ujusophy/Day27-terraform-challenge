5b1283c3468da9b3d4c531bd0fcdf7520cc43f44
Njoku Ujunwa Sophia <106748148+Ujusophy@users.noreply.github.com>
2024-09-26 15:30:26 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "zone_id" {
+  description = "The ID of the Route53 hosted zone"
+  default        = "###########C"
+}
+
+variable "domain_name" {
+  description = "The domain name for Route 53 records"
+  default     = "techynurse.site"
+}
